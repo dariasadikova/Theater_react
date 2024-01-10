8f50f14679225621a50a3de02a69c54b2a3a3071
daria <sadidash@yandex.ru>
2024-01-10 13:58:43 +0300

    обновила lab2, заменила прошлый апи на свой

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,105 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Divider, Table, Button } from 'antd';
+import { Table, Button, Input } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import './App.css'; 
+import './App.css';
+import playsData from './plays.json';
 
-interface DataType { 
-  name: string; 
-  country: string; 
-} 
+interface PlayType { 
+  id: number;
+  title: string; 
+  genre: string; 
+  director: string; 
+}
 
-const columns: ColumnsType<DataType> = [ 
-{ 
-  title: 'Name', 
-  dataIndex: 'name', 
-  key: 'name', 
-  render: (text) => <a >{text}</a>, 
-}, 
-{ 
-  title: 'Country', 
-  dataIndex: 'country', 
-  key: 'country', 
-}, 
+const columns: ColumnsType<PlayType> = [ 
+  { 
+    title: 'название', 
+    dataIndex: 'title', 
+    key: 'title', 
+  }, 
+  { 
+    title: 'жанр', 
+    dataIndex: 'genre', 
+    key: 'genre', 
+  }, 
+  { 
+    title: 'режиссер', 
+    dataIndex: 'director', 
+    key: 'director', 
+  }, 
 ]; 
 
-function App() { 
-  const LIMIT_LIST_SCHOOL = 10; 
-  const [page, setPage] = useState<number>(1) 
-  const [dataSource, setDataSource] = useState<DataType[]>() 
-  const getUniversity = async (page: number, limit: number) => { 
-      const response = await axios.get(`http://universities.hipolabs.com/search?offset=${page*limit}&limit=${limit}`) 
-      setDataSource(response.data);     
-  } 
+const App: React.FC = () => { 
+  const LIMIT = 10;
+  const [page, setPage] = useState<number>(1);
+  const [dataSource, setDataSource] = useState<PlayType[]>([]);
+  const [filterTitle, setFilterTitle] = useState<string>('');
+  const [filterGenre, setFilterGenre] = useState<string>('');
+  const [filterDirector, setFilterDirector] = useState<string>('');
+
+  const getFilteredData = () => {
+    return playsData.filter(play => 
+      play.title.toLowerCase().includes(filterTitle.toLowerCase()) &&
+      play.genre.toLowerCase().includes(filterGenre.toLowerCase()) &&
+      play.director.toLowerCase().includes(filterDirector.toLowerCase())
+    );
+  };
+
+  const getPlays = (page: number) => { 
+    const filteredData = getFilteredData();
+    const totalRecords = filteredData.length;
+    const maxPage = Math.ceil(totalRecords / LIMIT);
+    setPage(Math.min(page, maxPage));
+
+    const startIndex = (page - 1) * LIMIT;
+    const selectedPlays = filteredData.slice(startIndex, startIndex + LIMIT);
+    setDataSource(selectedPlays);
+  };
+
   useEffect(() => { 
-      getUniversity(page, LIMIT_LIST_SCHOOL) 
-  }, [page]) 
+    getPlays(page);
+  }, [page, filterTitle, filterGenre, filterDirector]);
+
+
+  const totalRecords = getFilteredData().length;
+  const maxPage = Math.ceil(totalRecords / LIMIT);
 
   return ( 
-      <> 
-          <Table dataSource={dataSource} columns={columns} pagination={false}/> 
-              <Button onClick={() => setPage(page - 1)} disabled={!(page-1)}>Назад</Button> 
-              <p>{page}</p> 
-              <Button onClick={() => setPage(page + 1)}>Вперед</Button> 
-      </> 
-  ) 
-} 
+    <div className="container">
+      <div className="table-and-filters">
+        <div className="table-container">
+          <Table dataSource={dataSource} columns={columns} pagination={false}/>
+          <div className="pagination">
+            <Button onClick={() => setPage(page - 1)} disabled={page === 1}>Назад</Button> 
+            <p>{page}</p> 
+            <Button onClick={() => setPage(page + 1)} disabled={page >= maxPage}>Вперед</Button> 
+          </div>
+        </div>
+        <div className="filters">
+          <p className="filters_h">фильтры</p>
+          <Input 
+            placeholder="Название" 
+            value={filterTitle} 
+            onChange={e => setFilterTitle(e.target.value)}
+          />
+          <Input 
+            placeholder="Жанр" 
+            value={filterGenre} 
+            onChange={e => setFilterGenre(e.target.value)}
+          />
+          <Input 
+            placeholder="Режиссер" 
+            value={filterDirector} 
+            onChange={e => setFilterDirector(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  ); 
+}
 
 export default App;
+
+
+
+
